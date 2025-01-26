84c0d755f21f1b8dd6a38249d4e0c2b4d4a72fb9
Рыбин Артем Олегович <a.o.rybin@novotelecom.ru>
2025-01-26 09:59:38 +0600

    Fix.

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,17 @@
 public class Main {
 
-    // Переменная для цикла
     final static int value = 100;
 
-    /* Метод вычисления кратности, на вход принимает число, если оно без остатка делится на 4
-     * метод вернет - true, иначе - false */
+    public static boolean isMultipleOfFour(int x) {
 
-    public static boolean function(int x) {
+        return x % 4 == 0;
 
-        if (x % 4 == 0) {
-            return true;
-        } else return false;
     }
-
 
     public static void main(String[] args) {
 
         for (int i = value; i > 0; i--) {
-            if (function(i)) {
+            if (isMultipleOfFour(i)) {
                 System.out.println(i);
             }
         }
